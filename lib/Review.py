8fcb3148b8ce18cad6043dfc12e5a08136061d41
AyimWilliam <odhisayim@gmail.com>
2023-06-05 01:38:45 +0300

    finished on reviews

--- lib/Review.py
+++ lib/Review.py
@@ -1,2 +1,23 @@
-class Review:
-    pass
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from base import Base, engine, session
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    rating = Column(Integer)
+
+    customer = relationship('Customer', backref='reviews')
+    restaurant = relationship('Restaurant', backref='reviews')
+
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+
+    @classmethod
+    def all(cls):
+        return session.query(cls).all()
