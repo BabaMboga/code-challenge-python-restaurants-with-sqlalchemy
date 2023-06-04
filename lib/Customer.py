b6bcd4353baa00da2789948cb3e0805e177fbd22
AyimWilliam <odhisayim@gmail.com>
2023-06-04 23:51:03 +0300

    progress made on customer.py

--- lib/Customer.py
+++ lib/Customer.py
@@ -8,3 +8,25 @@ class Customer(Base):
     id = Column(Integer, primary_key=True)
     given_name = Column(String)
     family_name = Column(String)
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+
+    def full_name(self):
+        return f"{self.given_name} {self.family_name}"
+    
+    def add_review(self, restaurant, rating):
+        review = Review(customer=self, restaurant=restaurant, rating=rating)
+        session.add(review)
+        session.commit()
+
+    def restaurants(self):
+        return [review.restaurant for review in self.reviews]
+    
+    def num_reviews(self):
+        return len(self.reviews)
+    
+    @classmethod
+    def all(cls):
+        return session.query(cls).all()
